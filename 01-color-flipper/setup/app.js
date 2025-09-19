7f7e5118af74d16d673b683bc30ea9be04f9f92e
NORDON01 <georges.nordon@gmail.com>
2025-09-19 14:45:55 +0200

    color-flipper app.js

--- 01-color-flipper/setup/app.js
+++ 01-color-flipper/setup/app.js
@@ -1,1 +1,22 @@
+const ititlog = console.log;
 const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+
+const btn = document.querySelector('.btn');
+const color = document.querySelector('.color');
+
+ btn.addEventListener('click', function(){
+    let randomNumber = tirage(); 
+    log(randomNumber); 
+    let randomColor = colors[randomNumber];
+    log(randomColor);
+    color.textContent=randomColor;
+    document.body.style.background = randomColor;
+}) 
+
+ function tirage(){
+    return Math.floor(Math.random()*4);
+}
+
+
+//log(Math.floor(Math.random()*4));
+
