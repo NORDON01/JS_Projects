5d616c330606fae521f15c180d39cee7353769eb
NORDON01 <georges.nordon@gmail.com>
2025-09-19 23:40:48 +0200

    projet 01-color-flipper terminé

--- 01-color-flipper/setup/app.js
+++ 01-color-flipper/setup/app.js
@@ -1,4 +1,4 @@
-const ititlog = console.log;
+const log = console.log;
 const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
 
 const btn = document.querySelector('.btn');
--- 01-color-flipper/setup/hex.js
+++ 01-color-flipper/setup/hex.js
@@ -1,1 +1,24 @@
+const log = console.log;
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+const btn = document.querySelector('.btn');
+const color = document.querySelector('.color');
+
+//Tirage au hasard 1 parmi 16, 6 fois
+//log(randomNumber);
+let nbreHex;
+
+function randomHexa (){
+nbreHex= '#';
+for(i =0; i < 6; i++){
+    let randomNumber = hex[Math.floor(Math.random()*hex.length)];
+    nbreHex = nbreHex + randomNumber;
+}
+log(nbreHex);
+return nbreHex;
+}
+
+btn.addEventListener('click', function(){
+    randomHexa();
+    document.body.style.backgroundColor = nbreHex;
+    color.textContent = nbreHex;
+})
