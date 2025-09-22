4b68acead1db1a8a5583b79fcc4db5bb68f0ea3e
NORDON01 <georges.nordon@gmail.com>
2025-09-22 21:25:11 +0200

    classList.toggle vu

--- 04-navbar/setup/app.js
+++ 04-navbar/setup/app.js
@@ -3,3 +3,20 @@
 // add - add class
 // remove - remove class
 // toggle - toggles class
+const log = console.log;
+
+const links = document.querySelector('.links');
+const hamburger = document.querySelector('.nav-toggle');
+
+ /*    hamburger.addEventListener('click', function(){
+    if(liste.classList.contains('show-links')){
+        liste.classList.remove('show-links')
+    }else{liste.classList.add('show-links')}
+}) */
+
+           /*  OU */
+           
+    hamburger.addEventListener('click', function(){
+        links.classList.toggle('show-links');            
+        log(links.classList);
+    })
