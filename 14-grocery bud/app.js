5005c36b5a517ad77b5d55a8de613a4a29b0af0f
NORDON01 <georges.nordon@gmail.com>
2025-10-17 17:26:13 +0200

    14-select elements in the DOM

--- 14-grocery bud/app.js
+++ 14-grocery bud/app.js
@@ -1,7 +1,17 @@
+const log = console.log;
 // ****** SELECT ITEMS **********
+const alert = document.querySelector(".alert");log(alert)
+const form = document.querySelector(".grocery-form");log(form);
+const grocery = document.getElementById("grocery");log(grocery);
+const submitBtn = document.querySelector(".grocery-btn");log(submitBtn);
+const container = document.querySelector(".grocery-container");log(container);
+const list = document.querySelector(".grocery-list");log(list);
+const clearBtn = document.querySelector(".clear-btn");log(clearBtn);
 
 // edit option
-
+let editElement;
+let editFlag = false;
+let editId = "";
 // ****** EVENT LISTENERS **********
 
 // ****** FUNCTIONS **********
